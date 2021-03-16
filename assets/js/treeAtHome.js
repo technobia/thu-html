9771800211d229c88e89f79281260e576033193f
BInh Tran <technobia@gmail.com>
2021-03-17 00:08:35 +0700

    fix responsive

--- assets/js/treeAtHome.js
+++ assets/js/treeAtHome.js
@@ -1,21 +1,25 @@
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  center: true,
-  margin: 50,
-  nav: false,
-  autoWidth: true,
-  autoplay: true,
-  autoplayTimeout: 5000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 3
-    },
-    1000: {
-      items: 5
+$(window).ready(() => {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    center: true,
+    nav: false,
+    autoWidth: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      640: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      1200: {
+        items: 5
+      }
     }
-  }
+  })
 })
